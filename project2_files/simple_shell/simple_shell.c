15e7d8767cd977368e04a5e8f6cc50ecc01cdc0f
James Harris <jharris7@umbc.edu>
2022-03-11 12:57:07 -0500

    Added user input and while loop

--- project2_files/simple_shell/simple_shell.c
+++ project2_files/simple_shell/simple_shell.c
@@ -5,13 +5,26 @@
 int main(int argc, char *argv[]){
 
   if (argc > 1){
-    fprintf(stderr, "[ERROR] \nThis shell does not accept any command line arguments! \nArgument: %s \n", argv[1]);
-
+    fprintf(stderr, "[ERROR] \nThis shell does not accept any command line arguments! \nArgument(s): ");
+    for (int i = 1; i < argc; i++){
+      fprintf(stderr, "%s ", argv[i]);
+    }
+    fprintf(stderr, "\n");
+    
     exit(1);
   }
 
   else{
     printf("This is a simple shell made by James Harris!\n");
+
+    while(1){
+      char *command;
+      size_t command_size = 0;
+      size_t chars_read;
+
+      printf(">> ");
+      chars_read = getline(&command, &command_size, stdin);      
+    }
   }
 
   return 0;
