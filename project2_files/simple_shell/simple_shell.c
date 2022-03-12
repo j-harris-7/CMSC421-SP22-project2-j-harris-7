9f17efd1682a55cba7601555eca415e32b44ebba
James Harris <jharris7@umbc.edu>
2022-03-11 21:09:06 -0500

    Almost done part 1, need some functionality and such

--- project2_files/simple_shell/simple_shell.c
+++ project2_files/simple_shell/simple_shell.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
 
@@ -18,12 +20,69 @@ int main(int argc, char *argv[]){
     printf("This is a simple shell made by James Harris!\n");
 
     while(1){
-      char *command;
+      char *command = NULL;
       size_t command_size = 0;
       size_t chars_read;
 
       printf(">> ");
-      chars_read = getline(&command, &command_size, stdin);      
+      chars_read = getline(&command, &command_size, stdin);
+
+      char *token = strtok(command, " ");
+
+      if (strcmp(token, "exit\n") == 0){
+	free(command);
+	command = NULL;
+	
+	printf("Exiting shell...\n");
+	exit(0);
+      }
+      
+      if (strcmp(token, "exit") == 0){
+	token = strtok(NULL, " ");
+		
+	if (isdigit(token[0]) != 0){
+	  int code = atoi(token);
+	  printf("Exiting shell with exit code %u...\n", code);
+	  
+	  free(command);
+	  command = NULL;
+	  
+	  exit(code);
+	}	
+      }
+      
+      char * command_arr[chars_read][chars_read + 1];
+      
+      int count = 0;
+      while (token != NULL){
+        strcpy(command_arr[count], token);
+        count++;
+        token = strtok(NULL, " ");
+      }
+      
+      for (int i = 0; i < count; i++){
+        printf("The command word is: %s\n", command_arr[i]);
+      }
+      
+      
+      /*if (strcmp(token, "ls\n") == 0){
+        if (fork() == 0){
+        
+          printf("ls command called\n");
+          char *binaryPath = "/bin/ls";
+          execl(binaryPath, binaryPath, NULL);
+          //make sure to handle if exec fails
+        }
+        wait(NULL);
+      }*/
+      
+      
+
+      // parse argument, call shell command
+      //char command_arr[][]
+
+      free(command);
+      command = NULL;
     }
   }
 
